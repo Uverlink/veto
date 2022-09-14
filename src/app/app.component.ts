2b9e87c7fdc6206eb5bd7cb2767106c9291cc47e
Uverlink <marc.pizzanelli@gmail.com>
2022-09-14 15:57:25 +0200

    Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'veto';
+export class AppComponent implements OnInit {
+  title = 'Super Veto +';
+  animalList = ['Chien',"Chat","Tigre"];
+
+  ngOnInit(): void {
+    console.table(this.animalList);
+  }
+
+  selectAnimal(animalName: string) {
+    console.log(`Vous avez sélectionné ${animalName}`);
+  }
 }
